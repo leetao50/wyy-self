fcc4e84a4359b831bd94f136f3dbd0156fe585e9
leetao <404822478@qq.com>
2020-10-12 22:50:57 +0800

    sd

---
+++ src/app/share/wy-ui/wy-player/wy-player-panel/wy-lyric.ts
@@ -0,0 +1,73 @@
+import { type } from 'os';
+import { Lyric } from 'src/app/services/data-types/common.types';
+
+export interface BaseLyricLine{
+    txt: string;
+    txtCn: string;
+}
+
+
+const tiemExp = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/;
+
+interface LyricLine extends BaseLyricLine{
+
+    time:number;
+}
+
+export class WyLyric{
+    private lrc:Lyric;
+    lines:LyricLine[] = [];
+    constructor(lrc:Lyric){
+        this.lrc = lrc;
+        this.init();
+    }
+
+    init(){
+        if(this.lrc.tlyric){
+            this.generTLyric();
+        }else{
+            this.generLyric();
+        }
+    }
+
+    private generTLyric() {
+        const lines = this.lrc.lyric.split('\n');
+        const tlines = this.lrc.tlyric.split('\n').filter(item=> tiemExp.exec(item)!===null);
+
+        const moreLine = lines.length - tlines.length;
+
+        let tempArr = [];
+        if(moreLine>=0){
+            tempArr =[lines,tlines];
+        }else{
+            tempArr = [tlines,lines];
+        }
+
+        const first = tiemExp.exec(tempArr[1][0])[0];
+
+    }
+
+    private generLyric() {
+        
+        const lines = this.lrc.lyric.split('\n');
+        lines.forEach(line=> this.makeLine(line));
+        
+    }
+
+    private makeLine(line:string) {
+        const result = tiemExp.exec(line);
+
+
+        if(result){
+            const txt = line.replace(tiemExp,'').trim();
+            const txtCn ='';
+            if(txt){
+                let thirdResult = result[3]||'0';
+                const len = thirdResult.length;
+                const _thirdResult = len>2?parseInt(thirdResult):parseInt(thirdResult)*10;
+                const time = Number(result[1])*60*1000 +Number(result[2])*1000+_thirdResult;
+                this.lines.push({txt, txtCn, time});
+            }
+        } 
+    }
+}
